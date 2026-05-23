a8d13dbf99901b550c060e14dd2da34767d05edd
KamilBugaj6512 <kamil.bugaj@student.wsb.edu.pl>
2026-05-23 22:24:19 +0200

    Add files via upload

---
+++ C#_ZADANIE_668851_SREDNIA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Console.WriteLine("Program do obliczania średniej ocen ucznia");
+
+Console.Write("Podaj ile ocen chcesz wprowadzic: ");
+int liczbaOcen = Convert.ToInt32(Console.ReadLine());
+double sumaOcen = 0;
+
+for (int i = 0; i < liczbaOcen; i++)
+{
+    Console.Write("Podaj kolejną ocenę (1-6): ");
+    double ocena = Convert.ToDouble(Console.ReadLine());
+    sumaOcen = sumaOcen + ocena;
+}
+
+double srednia = sumaOcen / liczbaOcen;
+Console.WriteLine("Średnia ocen wynosi: " + Math.Round(srednia, 2));
+
+if (srednia >= 3.0)
+{
+    Console.WriteLine("Uczeń zdał.");
+}
+else
+{
+    Console.WriteLine("Uczeń nie zdał.");
+}
